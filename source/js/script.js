d64c4872544977359fe1245776bfddc42f848235
MYiLA <polus180@ya.ru>
2020-03-05 00:01:23 +0300

    html mark is started

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const tabletWidth = 900;
+const desktopWidth = 1440;
 
 const closeAnimation = function (wrap) {
   wrap.classList.remove('dissolve-show');
--- source/js/video.js
+++
@@ -1,6 +0,0 @@
-const playBtnElement = document.querySelector('.media__play-btn');
-const videoCoverElement = document.querySelector('.media__wrap-desc');
-
-playBtnElement.addEventListener('click', () => {
-  closeAnimation(videoCoverElement);
-});
