b42c45f6c9a46f4092962ae201e68167ab47d133
MYiLA <polus180@ya.ru>
2020-03-22 22:54:10 +0300

    created block heder and  img directory

--- source/js/nav-menu.js
+++ source/js/nav-menu.js
@@ -3,6 +3,17 @@ $('.menu__burger-btn').on('click', function () {
   $('.navbar-collapse').toggleClass('show-menu');
 })
 
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1){
+    $('.header').addClass('header--fixed')
+    $('.header__tel').addClass('header__tel--fixed')
+  }
+  else{
+  $('.header').removeClass('header--fixed');
+  $('.header__tel').removeClass('header__tel--fixed')
+  }
+  });
+
 //анимация бургера
 $(document).ready(function () {
 
