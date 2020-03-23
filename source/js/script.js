409af0d7bd2f31b7221d89a10523d4c43c6f2d8b
MYiLA <polus180@ya.ru>
2020-03-23 21:00:51 +0300

    upcoming-trips block ready

--- source/js/script.js
+++ source/js/script.js
@@ -7,3 +7,19 @@ const closeAnimation = function (wrap) {
   wrap.classList.remove('dissolve-show');
   wrap.classList.add('dissolve-hidden');
 }
+
+
+// Плавный скролл ссылок-якорей
+
+$(document).ready(function () {
+  $('a').on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: ($(hash).offset().top) - 100
+      }, 800);
+    }
+  });
+});
---
+++ source/js/uncoming-trips.js
@@ -0,0 +1,79 @@
+// эффект ховера на мобильной ширине
+const widthMobile = 320;
+const widthTabletSmall = 660;
+const widthTablet = 900;
+const widthDesktop = 1440;
+const disappearCss = {
+    'opacity': 0,
+    'pointer-events': 'none'
+};
+const appearCss = {
+    'opacity': 1,
+    'pointer-events': 'auto'   
+}
+
+
+
+const uncomingHoverMobile = function () {
+    if ($(window).width() < widthTabletSmall) {
+        $('.upcoming-trips__item').each(function () {
+            $(this).children('.upcoming-trips__item-wrap').children('.upcoming-trips__btn-mobile').on('click', function () {
+                $(this).parent().parent().children('.upcoming-trips__item-hover').css({
+                    'transform': 'translateX(0)',
+                    'opacity': 1,
+                    'pointer-events': 'auto'
+                })
+            });
+
+            $(this).children('.upcoming-trips__item-hover').children('.upcoming-trips__btn-mobile').on('click', function () {
+                $(this).parent().css({
+                    'transform': 'translateX(75%)',
+                    'opacity': 0,
+                    'pointer-events': 'none'
+                })
+            });
+        });
+    } else {
+        $('.upcoming-trips__item').each(function () {
+            $(this).children('.upcoming-trips__item-hover').attr('style', '')
+        });
+    };
+}
+
+const collapseUncoming = function () {
+    if ($(window).width() < widthTablet) {
+        $('.upcoming-trips__btn-collapse--max').on('click', function () {
+            $('.upcoming-trips__btn-collapse--min').css('display', 'block');
+            $('.upcoming-trips__btn-collapse--max').css('display', 'none');
+            $('.upcoming-trips__item').css('display', 'block');
+        })
+
+        $('.upcoming-trips__btn-collapse--min').on('click', function () {
+            $('.upcoming-trips__btn-collapse--min').css('display', 'none');
+            $('.upcoming-trips__btn-collapse--max').css('display', 'block');
+            $('.upcoming-trips__item:not(:nth-child(1)):not(:nth-child(2))').css('display', 'none');
+        })
+        // $('.upcoming-trips__btn-collapse--min').hide();
+        // $('.upcoming-trips__btn-collapse--max').show("slow");
+    } else {
+        $('.upcoming-trips__item').each(function () {
+            $(this).attr('style', '');
+        })
+        $('.upcoming-trips__btn-collapse--min').attr('style', '');
+        $('.upcoming-trips__btn-collapse--max').attr('style', '');
+    }
+}
+
+$(window).resize(function () {
+    uncomingHoverMobile()
+    if($(window).width() >= widthTablet) {
+        $('.upcoming-trips__item').each(function () {
+            $(this).attr('style', '');
+        })
+        $('.upcoming-trips__btn-collapse--min').attr('style', '');
+        $('.upcoming-trips__btn-collapse--max').attr('style', '');
+    }
+})
+
+uncomingHoverMobile()
+collapseUncoming()
