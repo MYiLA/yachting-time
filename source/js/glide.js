934cebd4d74d0a3d37ea7fc11c7147b2738abb51
MYiLA <polus180@ya.ru>
2020-03-29 05:51:12 +0300

    impressions html created

--- source/js/glide.js
+++ source/js/glide.js
@@ -17,7 +17,7 @@ const alsoSlider = new Glide('.also__glide', {
             perView: 1
         }
     },
-}).mount();
+});
 
 const introSlider = new Glide('.intro__glide', {
     type: 'carousel',
@@ -39,8 +39,62 @@ const introSlider = new Glide('.intro__glide', {
             perView: 1
         }
     },
-}).mount();
+});
 
+const croatiaSlider = new Glide('.impressions__glide--croatia', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    gap: 30,
+    peek: {before: 50, after: 50},
+    autoplay: 5000,
+    animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
+    dragThreshold: 50,
+    animationDuration: 1000,
+});
+
+const greeceSlider = new Glide('.impressions__glide--greece', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    gap: 30,
+    peek: {before: 50, after: 50},
+    autoplay: 5000,
+    animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
+    dragThreshold: 50,
+    animationDuration: 1000,
+});
+
+const sicilySlider = new Glide('.impressions__glide--sicily', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    gap: 30,
+    peek: {before: 50, after: 50},
+    autoplay: 5000,
+    animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
+    dragThreshold: 50,
+    animationDuration: 1000,
+});
+
+const turkeySlider = new Glide('.impressions__glide--turkey', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    gap: 30,
+    peek: {before: 50, after: 50},
+    autoplay: 5000,
+    animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
+    dragThreshold: 50,
+    animationDuration: 1000,
+});
+
+introSlider.mount();
+alsoSlider.mount();
+croatiaSlider.mount();
+greeceSlider.mount();
+sicilySlider.mount();
+turkeySlider.mount();
 
 const deleteAlsoSlider = function (slider, width) {
     if(window.innerWidth >= width) {
