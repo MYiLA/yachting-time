74662a6cff7943c2023870d6c278c4a286d943dd
MYiLA <polus180@ya.ru>
2020-03-30 07:42:26 +0300

    created block impression

--- source/js/impression.js
+++ source/js/impression.js
@@ -2,51 +2,203 @@
 // слайдеры
 
 const croatiaSlider = new Glide('.impressions__glide--croatia', {
-  type: 'carousel',
+  type: 'slider',
   startAt: 0,
   perView: 1,
   gap: 30,
-  // peek: {before: 50, after: 50},
+  peek: {before: 150, after: 150},
   autoplay: 5000,
   animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
   dragThreshold: 50,
   animationDuration: 1000,
+  breakpoints: {
+    1800: {
+      peek: {before: 420, after: 420},
+    },
+    1700: {
+      peek: {before: 300, after: 300},
+    },
+    1440: {
+      peek: {before: 240, after: 240},
+    },
+    1340: {
+      peek: {before: 250, after: 250},
+    },
+    1170: {
+      peek: {before: 180, after: 180},
+    },
+    1000: {
+      peek: {before: 130, after: 130},
+    },
+    900: {
+      peek: {before: 280, after: 280},
+    },
+    800: {
+      peek: {before: 240, after: 240},
+    },
+    700: {
+      peek: {before: 180, after: 180},
+    },
+    600: {
+      peek: {before: 140, after: 140},
+    },
+    510: {
+      peek: {before: 100, after: 100},
+    },
+    420: {
+      peek: {before: 0, after: 90},
+    }
+},
 });
 
 const greeceSlider = new Glide('.impressions__glide--greece', {
-  type: 'carousel',
+  type: 'slider',
   startAt: 0,
   perView: 1,
   gap: 30,
-  // peek: {before: 50, after: 50},
+  peek: {before: 150, after: 150},
   autoplay: 5000,
   animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
   dragThreshold: 50,
   animationDuration: 1000,
+  breakpoints: {
+    1800: {
+      peek: {before: 420, after: 420},
+    },
+    1700: {
+      peek: {before: 300, after: 300},
+    },
+    1440: {
+      peek: {before: 240, after: 240},
+    },
+    1340: {
+      peek: {before: 250, after: 250},
+    },
+    1170: {
+      peek: {before: 180, after: 180},
+    },
+    1000: {
+      peek: {before: 130, after: 130},
+    },
+    900: {
+      peek: {before: 280, after: 280},
+    },
+    800: {
+      peek: {before: 240, after: 240},
+    },
+    700: {
+      peek: {before: 180, after: 180},
+    },
+    600: {
+      peek: {before: 140, after: 140},
+    },
+    510: {
+      peek: {before: 100, after: 100},
+    },
+    420: {
+      peek: {before: 0, after: 90},
+    }
+},
 });
 
 const sicilySlider = new Glide('.impressions__glide--sicily', {
-  type: 'carousel',
+  type: 'slider',
   startAt: 0,
   perView: 1,
   gap: 30,
-  // peek: {before: 50, after: 50},
+  peek: {before: 150, after: 150},
   autoplay: 5000,
   animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
   dragThreshold: 50,
   animationDuration: 1000,
+  breakpoints: {
+    1800: {
+      peek: {before: 420, after: 420},
+    },
+    1700: {
+      peek: {before: 300, after: 300},
+    },
+    1440: {
+      peek: {before: 240, after: 240},
+    },
+    1340: {
+      peek: {before: 250, after: 250},
+    },
+    1170: {
+      peek: {before: 180, after: 180},
+    },
+    1000: {
+      peek: {before: 130, after: 130},
+    },
+    900: {
+      peek: {before: 280, after: 280},
+    },
+    800: {
+      peek: {before: 240, after: 240},
+    },
+    700: {
+      peek: {before: 180, after: 180},
+    },
+    600: {
+      peek: {before: 140, after: 140},
+    },
+    510: {
+      peek: {before: 100, after: 100},
+    },
+    420: {
+      peek: {before: 0, after: 90},
+    }
+},
 });
 
 const turkeySlider = new Glide('.impressions__glide--turkey', {
-  type: 'carousel',
+  type: 'slider',
   startAt: 0,
   perView: 1,
   gap: 30,
-  // peek: {before: 50, after: 50},
+  peek: {before: 150, after: 150},
   autoplay: 5000,
   animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
   dragThreshold: 50,
   animationDuration: 1000,
+  breakpoints: {
+    1800: {
+      peek: {before: 420, after: 420},
+    },
+    1700: {
+      peek: {before: 300, after: 300},
+    },
+    1440: {
+      peek: {before: 240, after: 240},
+    },
+    1340: {
+      peek: {before: 250, after: 250},
+    },
+    1170: {
+      peek: {before: 180, after: 180},
+    },
+    1000: {
+      peek: {before: 130, after: 130},
+    },
+    900: {
+      peek: {before: 280, after: 280},
+    },
+    800: {
+      peek: {before: 240, after: 240},
+    },
+    700: {
+      peek: {before: 180, after: 180},
+    },
+    600: {
+      peek: {before: 140, after: 140},
+    },
+    510: {
+      peek: {before: 100, after: 100},
+    },
+    420: {
+      peek: {before: 0, after: 90},
+    }
+},
 });
 // слайдеры
 // croatiaSlider.mount();
@@ -74,12 +226,8 @@ const turkeySlider = new Glide('.impressions__glide--turkey', {
 // слайдер по умолчанию
 croatiaSlider.mount();
 // табы стран
-const deleteActivImpressions = function (a) {
-  console.log(a);
-  console.log('croatia ' + $('.impressions__tab--croatia').hasClass('active'))
-  console.log('greece ' + $('.impressions__tab--greece').hasClass('active'))
-  console.log('sicil ' + $('.impressions__tab--sicily').hasClass('active'))
-  console.log('turkey ' + $('.impressions__tab--turkey').hasClass('active'))
+const deleteActivImpressions = function () {
+  
   if ($('.impressions__tab--croatia').hasClass('active')) {
     $('.impressions__tab--croatia').removeClass('active');
     $('.impressions__glide--croatia').removeClass('active');
@@ -88,7 +236,6 @@ const deleteActivImpressions = function (a) {
   else if ($('.impressions__tab--greece').hasClass('active')) {
     $('.impressions__tab--greece').removeClass('active');
     $('.impressions__glide--greece').removeClass('active');
-    console.log(a);
     greeceSlider.destroy();
   }
   else if ($('.impressions__tab--sicily').hasClass('active')) {
@@ -108,25 +255,30 @@ $('.impressions__tab').each(function () {
     console.log('нажали ' + $(this));
     deleteActivImpressions($(this));
     console.log('работает ' + $(this));
+    // setTimeout(function(){}, 0)
     $(this).addClass('active');
     if ($('.impressions__tab--croatia').hasClass('active')) {
       $('.impressions__glide--croatia').addClass('active');
       croatiaSlider.mount();
+      // $('.glide__slide--clone').remove();
     }
 
     else if ($('.impressions__tab--greece').hasClass('active')) {
       $('.impressions__glide--greece').addClass('active');
       greeceSlider.mount();
+      // $('.glide__slide--clone').remove();
     }
 
     else if ($('.impressions__tab--sicily').hasClass('active')) {
       $('.impressions__glide--sicily').addClass('active');
       sicilySlider.mount();
+      // $('.glide__slide--clone').remove();
     }
 
     else if ($('.impressions__tab--turkey').hasClass('active')) {
       $('.impressions__glide--turkey').addClass('active');
       turkeySlider.mount();
+      // $('.glide__slide--clone').remove();
     }
   })
 })
