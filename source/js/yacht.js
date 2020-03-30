f5f243216951298553b518cf0d035a09da2d26cf
MYiLA <polus180@ya.ru>
2020-03-31 02:30:28 +0300

    Yacht block created

---
+++ source/js/yacht.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const yachtSlider = new Glide('.yacht-glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    gap: 30,
+    autoplay: 5000,
+    animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)',
+    dragThreshold: 50,
+    animationDuration: 1000,
+});
+
+yachtSlider.mount();
+
+$(function() {
+  $("a").each(function(b) {//работа с элементом (ссылка)
+      if (this.title) {
+          var c = this.title;
+          var x = 0;//расположение по горизонтали(left)
+          var y = 35;//расположение по вертикали (top)
+          $(this).mouseover(function(d) {
+              this.title = "";
+              $("body").append('<div id="tooltip">' + c + "</div>");
+              $("#tooltip").css({
+                  left: (d.pageX + x) + "px",
+                  top: (d.pageY + y) + "px",
+                  opacity: "1"//полупрозрачность
+              }).show(600)//скорость появления подсказки
+          }).mouseout(function() {
+              this.title = c;
+              $("#tooltip").remove()
+          }).mousemove(function(d) {
+              $("#tooltip").css({
+                  left: (d.pageX + x) + "px",
+                  top: (d.pageY + y) + "px"
+              })
+          })
+      }
+  })
+  });
